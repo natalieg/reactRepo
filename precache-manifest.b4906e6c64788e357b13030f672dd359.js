85f1534028f6edffa1747c08c972f7a5241e3ff1
Natalie-Gornicki <natalie.gornicki@gmail.com>
2019-10-30 10:58:45 +0100

    Updates

---
+++ precache-manifest.b4906e6c64788e357b13030f672dd359.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86cf1d7333514f68646bda05d51e9b1b",
+    "url": "/reactRepo/index.html"
+  },
+  {
+    "revision": "560b6643450661981137",
+    "url": "/reactRepo/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "5bd14360b95d228df49d",
+    "url": "/reactRepo/static/js/2.04ae8c2c.chunk.js"
+  },
+  {
+    "revision": "560b6643450661981137",
+    "url": "/reactRepo/static/js/main.119f32da.chunk.js"
+  },
+  {
+    "revision": "81ce6b64488197c2637e",
+    "url": "/reactRepo/static/js/runtime-main.3f44af96.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/reactRepo/static/media/logo.25bf045c.svg"
+  }
+]);
